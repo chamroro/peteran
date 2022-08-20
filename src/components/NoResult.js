a9a5f6dff05808fcdef2eaa7600cb9d244796b80
chamroro <chamsae@handong.ac.kr>
2022-08-21 08:25:39 +0900

    noresult

---
+++ src/components/NoResult.js
@@ -0,0 +1,18 @@
+import {No} from '../../assets/noresult.png';
+import Text from "../Text";
+
+function NoResult(){
+    
+    return(
+        <div>
+            <img src={No}
+                alt={'No result!'}
+            style={{width: 80, height: 80}} />
+            <Text n='s1' text="검색 결과가 없습니다"/>
+        </div>
+    );
+
+}
+
+export default NoResult;
+
